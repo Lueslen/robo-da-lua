0a43d3821bcc2f0d852d455e3cc9a2a29f7c802d
Lueslen Machado da Silva <lueslensilva@hotmail.com>
2022-05-23 21:55:21 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Robo robo = new Robo(100);
+    robo.viraDireita(70);
+    robo.andaFrente(10);
+    robo.viraEsquerda(165);
+    robo.andaAtras(40);
+    robo.fotinho();
+    robo.getStatus();
+      }
+}
